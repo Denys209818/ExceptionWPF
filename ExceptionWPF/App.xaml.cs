b7f8369373209342c283ae7be1e60e8cd3ca6b8f
elik <elik@example.com>
2021-03-18 19:02:50 +0200

    Load files

---
+++ ExceptionWPF/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExceptionWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        ///     Перевантажений метод DispatcherUnhandledException, який відловлює усі помилки,
+        ///     відловлювання яких було відсутнє в коді. 
+        /// </summary>
+        /// <param name="sender">Обєкт який згенерував подію</param>
+        /// <param name="e">Параметри помилки</param>
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            //  Присвоєння помилки до нової змінної
+            Exception ex = e.Exception;
+            //  Виведення вікна, яке сповіщає про помилку в коді
+            MessageBox.Show("An unhandled exception just occurred: " 
+                + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            //  Параметр Handled повідомляє програмі що помилку вже оброблено і програмі не потрібно
+            //  її додатково обролювати
+            e.Handled = true;
+        }
+    }
+}
---
+++ ExceptionWPF/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExceptionWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///     Подія яка оброблює клік кнопки
+        /// </summary>
+        /// <param name="sender">Кнопка яка згенерувала подію</param>
+        /// <param name="e">Параметри</param>
+        private void btnException_Click(object sender, RoutedEventArgs e)
+        {
+            //  Ініціалізація нової строки і присвоєння їй NULL
+            string s = null;
+            try
+            {   
+                //  Виконування операції над елементом який дорівнює null
+                //  Вивлети Exception, яка обробиться локальним обробником try... catch..
+                s.Trim();
+            }
+            //  Відловлювання помилки
+            catch (Exception ex) 
+            {
+                //  Виведення вікна, яке сповіщає про помилку яка сталася у коді,
+                //  використовуючи обєкт помилки. У вікні є лише одна кнопка і тип зображення вікна змінено на
+                //  MessageBoxImage.Warning
+                MessageBox.Show("A handled exception just occurred: " + 
+                    ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            //  Виконування операції над елементом, якому присвоєно NULL
+            s.Trim();
+        }
+    }
+}
